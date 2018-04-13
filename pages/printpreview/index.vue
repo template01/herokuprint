9fd85a3e093ef4822eff8cf2f31a8f6248afa9ea
lasbian <lasbian@debian.home>
2018-04-13 15:04:49 +0200

    test with reinvented

--- pages/printpreview/index.vue
+++ pages/printpreview/index.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="">
   <div class="">
-    <div v-for="item in content.slice(0, 250)" >
+    <div v-for="item in content.slice(0, 250)">
       <h1 class="is-size-3" v-html="item.title.rendered"></h1>
-      <div class="is-size-5" v-html="item.content.rendered"></div>
+      <div class="is-size-5" v-html="parsedContent(item.content.rendered)"></div>
     </div>
   </div>
 
@@ -22,14 +22,26 @@ import {
 
 
 export default {
-  data: function(){
-    return{
-      images: ['https://farm9.staticflickr.com/8319/29652800220_1211078d8e_k_d.jpg','https://farm3.staticflickr.com/2807/33102046353_276e9a8809_o_d.jpg','https://farm5.staticflickr.com/4075/4858564088_b13e623117_o_d.jpg','https://farm3.staticflickr.com/2822/33078988203_ff60fe8f26_k_d.jpg','https://farm6.staticflickr.com/5793/30474312970_19ccff5aec_o_d.jpg','https://farm4.staticflickr.com/3716/9467361414_98c88b0289_o_d.jpg','https://farm5.staticflickr.com/4135/4858566784_aa5e1d3003_o_d.jpg','https://farm9.staticflickr.com/8886/18279455859_28247e0a16_k_d.jpg','https://farm1.staticflickr.com/901/27334117638_d984193f4c_o_d.jpg','https://farm6.staticflickr.com/5342/9467415240_ff29751590_o_d.jpg','https://farm8.staticflickr.com/7341/9458071103_2cabc5aeff_o_d.jpg','https://farm8.staticflickr.com/7288/9457398131_1f267a3b5c_o_d.jpg']
-  }
-},
+  data: function() {
+    return {
+      images: ['https://farm9.staticflickr.com/8319/29652800220_1211078d8e_k_d.jpg', 'https://farm3.staticflickr.com/2807/33102046353_276e9a8809_o_d.jpg', 'https://farm5.staticflickr.com/4075/4858564088_b13e623117_o_d.jpg',
+        'https://farm3.staticflickr.com/2822/33078988203_ff60fe8f26_k_d.jpg', 'https://farm6.staticflickr.com/5793/30474312970_19ccff5aec_o_d.jpg', 'https://farm4.staticflickr.com/3716/9467361414_98c88b0289_o_d.jpg',
+        'https://farm5.staticflickr.com/4135/4858566784_aa5e1d3003_o_d.jpg', 'https://farm9.staticflickr.com/8886/18279455859_28247e0a16_k_d.jpg', 'https://farm1.staticflickr.com/901/27334117638_d984193f4c_o_d.jpg',
+        'https://farm6.staticflickr.com/5342/9467415240_ff29751590_o_d.jpg', 'https://farm8.staticflickr.com/7341/9458071103_2cabc5aeff_o_d.jpg', 'https://farm8.staticflickr.com/7288/9457398131_1f267a3b5c_o_d.jpg'
+      ]
+    }
+  },
   components: {
     genericcomp,
     postlistcomp
+  },
+  methods: {
+    parsedContent: function(input) {
+      var mystring = "hello world test world";
+      var find = "http://wdka-pp.template-studio.nl/";
+      var regex = new RegExp(find, "g");
+      return input.replace(regex, "http://reinvented.wdka.nl/backend/")
+    }
   },
 
 
@@ -42,8 +54,12 @@ export default {
     redirect
   }) {
 
-      let { data } = await axios.get('http://reinvented.wdka.nl/backend/wp-json/wp/v2/posts?per_page=100')
-      return { content: data }
+    let {
+      data
+    } = await axios.get('http://reinvented.wdka.nl/backend/wp-json/wp/v2/posts?per_page=100')
+    return {
+      content: data
+    }
 
   },
 
